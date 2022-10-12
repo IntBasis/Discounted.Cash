2cf1b1e93af85330804564d3a67d858b2b5e0704
Jacob Foshee <jacobf@gmail.com>
2022-10-12 13:23:14 -0500

    refactor: Extract `ToDouble` function

---
+++ Parsing.cs
@@ -0,0 +1,11 @@
+﻿namespace IntBasis.Apps.DiscountedCash;
+
+public static class Parsing
+{
+    public static double ToDouble(object? value)
+    {
+        if (string.IsNullOrWhiteSpace(value as string))
+            return 0;
+        return Convert.ToDouble(value);
+    }
+}
